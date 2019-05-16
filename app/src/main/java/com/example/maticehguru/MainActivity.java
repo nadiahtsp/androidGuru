9bf06edc4ee6f7a2586597eed94e4678efcb8dc0
Asymala <asymalapsari@gmail.com>
2019-05-16 12:11:23 +0700

    navbar guru

--- app/src/main/java/com/example/maticehguru/MainActivity.java
+++ app/src/main/java/com/example/maticehguru/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.maticehguru;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView user_bottom_nav = findViewById(R.id.user_bottomnavigation);
+        user_bottom_nav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.guru_fragment_container,
+                new GuruFragmentHome()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.guru_bottomnav_home:
+                            selectedFragment = new GuruFragmentHome();
+                            break;
+                        case R.id.guru_bottomnav_scan:
+                            selectedFragment = new GuruScanFragment();
+                            break;
+                        case R.id.guru_bottomnav_history:
+                            selectedFragment = new GuruHistoryFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.guru_fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
