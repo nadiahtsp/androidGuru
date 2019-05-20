811b78ddf880a7deca8ee59dc5e5e670bc15c83f
pilotrogue <pilotrogue@gmail.com>
2019-05-20 19:16:01 +0700

    update gitignore

--- app/src/main/java/com/example/maticehguru/GuruFragmentHome.java
+++ app/src/main/java/com/example/maticehguru/GuruFragmentHome.java
@@ -21,52 +21,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GuruFragmentHome extends Fragment {
-    private Button scan_btn;
-    private Context context;
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null){
-            if (((IntentResult) result).getContents() == null ){
-                Toast.makeText(getActivity(),"Cancelled", Toast.LENGTH_LONG).show();
-
-            }
-            else {
-            Toast.makeText(getActivity(), ((IntentResult) result).getContents(),Toast.LENGTH_LONG).show();
-            }
-        }
-        else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-
-
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.user_fragment_home, container, false);
-        Button scan_btn = view.findViewById(R.id.scan_btn);
-        context = getActivity();
-        scan_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                IntentIntegrator integrator = new IntentIntegrator(getActivity());
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.setPrompt("Scan");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(false);
-                integrator.setBarcodeImageEnabled(false);
-                integrator.initiateScan();
-            }
-        });
-
-
         return view;
     }
-
-
-
 }
