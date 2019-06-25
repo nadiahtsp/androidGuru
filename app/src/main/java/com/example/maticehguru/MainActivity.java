339d69f00d405e7a2f541b2eea1b93ae97e9601c
priskaput <46365650+priskaput@users.noreply.github.com>
2019-06-25 22:57:39 +0700

    layout guru priska

--- app/src/main/java/com/example/maticehguru/MainActivity.java
+++ app/src/main/java/com/example/maticehguru/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.maticehguru;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,8 +44,8 @@ public class MainActivity extends AppCompatActivity {
                         case R.id.guru_bottomnav_history:
                             selectedFragment = new GuruHistoryFragment();
                             break;
-                        case R.id.guru_bottomnav_listPesanan:
-                            selectedFragment = new GuruListPesananFragment();
+                        case R.id.guru_bottomnav_profile:
+                            selectedFragment = new ProfileFragment();
                             break;
                         case R.id.guru_bottomnav_notification:
                             selectedFragment = new GuruNotifikasiFragment();
---
+++ app/src/main/java/com/example/maticehguru/ProfileFragment.java
@@ -0,0 +1,30 @@
+package com.example.maticehguru;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
+    }
+}
